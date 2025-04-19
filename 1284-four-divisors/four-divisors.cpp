5d573fc85f0469cd65670e881580a1d8efe940f2
Teena_25 <153281717+Teenakumawat123@users.noreply.github.com>
2025-04-19 17:56:39 +0530

    Time: 54 ms (47.86%) | Memory: 16.2 MB (77.88%) - LeetSync

--- 1284-four-divisors/four-divisors.cpp
+++ 1284-four-divisors/four-divisors.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
 int FourDiv(int x){
+    if((int)sqrt(x)*(int)sqrt(x)==x) return 0;
     int a=0,count=0;
-    for(int i=1;i<=sqrt(x);i++){
+    for(int i=1;i<sqrt(x);i++){
         if(x%i==0){
-           if(i==x/i){
-            a+=i;
-            count++;
-           }
-           else{
-             a+=i;
+           a+=i;
             a+=(x/i);
             count+=2;
-           }
         }
     }
     if(count==4) return a;
