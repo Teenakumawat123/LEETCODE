eac5091c5af04da3302ad21a51e265c6e0c3365e
Teena_25 <153281717+Teenakumawat123@users.noreply.github.com>
2025-04-27 10:49:05 +0530

    Time: 3 ms (15.71%) | Memory: 20.1 MB (9.01%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        vector<int>ans;
-        int n=nums.size();
+        unordered_map<int,int>mp;//create a map to store the frequency of each element 
+        vector<int>ans; // create a vector to store final answer
+        int n=nums.size();// size of given nums 
+
+        //count the frequency of each element 
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
+        //-->find elements whose frequency is greater than (n/3)
         for(auto t :mp){
-          if(t.second>(n/3)) ans.push_back(t.first);
+          if(t.second>(n/3)) ans.push_back(t.first); // if frequency is >(n/3),add it to the answer
         }
+        // return the final answer
         return ans;
     }
 };
