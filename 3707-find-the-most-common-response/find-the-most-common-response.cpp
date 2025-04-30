7158a0b3dd28e4bb4f03d051f75de518f1bed763
Teena_25 <153281717+Teenakumawat123@users.noreply.github.com>
2025-04-30 08:16:38 +0530

    Time: 1567 ms (22.76%) | Memory: 566.2 MB (13.11%) - LeetSync

---
+++ 3707-find-the-most-common-response/find-the-most-common-response.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string findCommonResponse(vector<vector<string>>& res) {
+        int n=res.size();
+         vector<string>temp;
+        for(int i=0;i<res.size();i++){
+            set<string>s;
+            // vector<string>temp;
+            for(int j=0;j<res[i].size();j++){
+             s.insert(res[i][j]);
+            }
+            for(string ele:s){
+                temp.push_back(ele);
+            }
+        }
+        map<string,int>mp;
+        for(int i=0;i<temp.size();i++){
+            mp[temp[i]]++;
+        }
+
+        int max_freq=INT_MIN;
+        for(auto p:mp){
+            max_freq=max(max_freq,p.second);
+        }
+        string result="";
+        for(auto p:mp){
+            if(p.second==max_freq){
+             result+=p.first;
+             break;
+            }
+        }
+        return result;
+    }
+};
