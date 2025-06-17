16f3306ff3af420d0725eba0b67e341bf0a58a0e
Teena_25 <153281717+Teenakumawat123@users.noreply.github.com>
2025-06-17 17:27:04 +0530

    Time: 29 ms (37.66%) | Memory: 85.6 MB (45.75%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -5,6 +5,7 @@ public:
        vector<int>pre(n);
        vector<int>next(n);
         stack<int>st;
+
         pre[0]=-1;
         st.push(0);
         for(int i=1;i<n;i++){
@@ -13,7 +14,9 @@ public:
             else pre[i]=st.top();
             st.push(i);
         }
+
         while(st.size()>0) st.pop();
+
         next[n-1]=n;
         st.push(n-1);
         for(int i=n-2;i>=0;i--){
@@ -22,10 +25,12 @@ public:
             else next[i]=st.top();
             st.push(i); 
         }
+
         int max_area=INT_MIN;
         for(int i=0;i<n;i++){
             max_area=max(max_area,h[i]*(next[i]-pre[i]-1));
         }
+        
         return max_area;
     }
 };
