690d49874fd65756edb0a8a9812f88eea99c18f3
Teena_25 <153281717+Teenakumawat123@users.noreply.github.com>
2025-07-08 12:40:18 +0530

    Time: 7 ms (50.84%) | Memory: 14.2 MB (38.53%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -6,7 +6,7 @@ public:
         stack<char>st;
         st.push(s[0]);
         for(int i=1;i<s.size();i++){
-            if(st.size()>0 && st.top()==s[i]) st.pop();
+            if(!st.empty() && st.top()==s[i]) st.pop();
             else st.push(s[i]);
         }
         if(st.size()==0) return "";
