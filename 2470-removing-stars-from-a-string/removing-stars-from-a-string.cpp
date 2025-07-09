6f02221858d3689b99d343efaab6c19d299000ff
Teena_25 <153281717+Teenakumawat123@users.noreply.github.com>
2025-07-09 06:29:28 +0530

    Time: 34 ms (10.64%) | Memory: 29.9 MB (14.47%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
     string removeStars(string s) {
-        if(s.size()==0) return "";
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]!='*') st.push(s[i]);
-            else{//s[i]=='*'
-            st.pop();
+        // If the input string is empty, return an empty string
+        if (s.size() == 0) return "";
+
+        // Stack to store characters that are not '*'
+        stack<char> st;
+
+        // Iterate through each character in the string
+        for (int i = 0; i < s.size(); i++) {
+            // If current character is not '*', push it to the stack
+            if (s[i] != '*') {
+                st.push(s[i]);
+            } else {
+                // If current character is '*', remove the top character from the stack
+                // This simulates "removing" the character before '*'
+                if (!st.empty()) st.pop();
             }
         }
-        if(st.size()==0) return "";
-        string result="";
-        while(st.size()>0){
-            char ch=st.top();
-            result+=ch;
+
+        // If the stack is empty, return an empty string
+        if (st.empty()) return "";
+
+        // Create a string to store the final result
+        string result = "";
+
+        // Pop characters from the stack and append to result string
+        while (!st.empty()) {
+            char ch = st.top();
+            result += ch;
             st.pop();
         }
-        reverse(result.begin(),result.end());
+
+        // The characters were added in reverse order, so reverse the string
+        reverse(result.begin(), result.end());
+
+        // Return the final string after removing stars and reversing
         return result;
     }
 };
