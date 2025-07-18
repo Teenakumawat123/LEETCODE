e1d1318dbba7fc23506c82cd52105cf2b83d6501
Teena_25 <153281717+Teenakumawat123@users.noreply.github.com>
2025-07-18 09:36:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (40.67%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,15 +11,26 @@
  */
 class Solution {
 public:
-  void solve(TreeNode* root,vector<int>&ans){
-    if(root==NULL) return;
-    ans.push_back(root->val);
-    solve(root->left,ans);
-    solve(root->right,ans);
-  }
+//   void solve(TreeNode* root,vector<int>&ans){
+//     if(root==NULL) return;
+//     ans.push_back(root->val);
+//     solve(root->left,ans);
+//     solve(root->right,ans);
+//   }
     vector<int> preorderTraversal(TreeNode* root) {
+       //iterative approach
+       stack<TreeNode*>st;
         vector<int>ans;
-        solve(root,ans);
+        if(root==NULL) return {};
+        st.push(root);
+        while(!st.empty()){
+            TreeNode*t=st.top();
+            st.pop();
+            ans.push_back(t->val);
+            if(t->right) st.push(t->right);
+            if(t->left) st.push(t->left);
+        }
+        // solve(root,ans);
         return ans;
     }
 };
