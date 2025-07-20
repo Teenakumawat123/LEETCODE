e924e5d6f71a10aac695d9e66f3a9920786d51c8
Teena_25 <153281717+Teenakumawat123@users.noreply.github.com>
2025-07-20 22:00:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (11.54%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -11,27 +11,21 @@
  */
 class Solution {
 public:
-void solve(TreeNode* root,string s,vector<string>&v){
+void solve(TreeNode* root,string s,int &sum){
     if(root==NULL) return;
     string a=to_string(root->val);
     if(root->left==NULL && root->right==NULL) {
         s+=a;
-        v.push_back(s);
+        sum+=stoi(s);
         return;
     }
-   solve(root->left,s+a,v);
-   solve(root->right,s+a,v);
+   solve(root->left,s+a,sum);
+   solve(root->right,s+a,sum);
 }
     int sumNumbers(TreeNode* root) {
-        vector<string>v;
-        solve(root,"",v);
-        for(int i=0;i<v.size();i++){
-            cout<<v[i]<<" ";
-        }
+        //vector<string>v;
         int sum=0;
-        for(int i=0;i<v.size();i++){
-            sum+=stoi(v[i]);
-        }
+        solve(root,"",sum);
         return sum;
     }
 };
