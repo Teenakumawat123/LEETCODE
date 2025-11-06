651b929a9a903c91d781ca86d97668f78e35d6fb
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2025-11-06 10:49:31 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (79.92%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int lo=0;
         int hi=nums.size()-1;
         while(lo<=hi){
-            int mid=(lo+hi)/2;
+            int mid=lo+(hi-lo)/2;
             if(nums[mid]==target) return mid;
             else if(nums[mid]>target) hi=mid-1;
             else lo=mid+1;
