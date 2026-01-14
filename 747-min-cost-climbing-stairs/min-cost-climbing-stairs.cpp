98ee164dcf42bf1a11f5550f07dcb4c47cb69813
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-01-14 15:37:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (99.77%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    int helper(vector<int>&cost,int i,vector<int>&dp){
-        if(i==0 || i==1) return cost[i];
-        if(dp[i]!=-1) return dp[i];
-        int sum1=helper(cost,i-1,dp);
-        int sum2=helper(cost,i-2,dp);
-        // if(dp[i]!=-1) return dp[i];
-        dp[i]=cost[i]+min(sum1,sum2);
-        return dp[i];
-    }
+    // int helper(vector<int>&cost,int i,vector<int>&dp){
+    //     if(i==0 || i==1) return cost[i];
+    //     if(dp[i]!=-1) return dp[i];
+    //     int sum1=helper(cost,i-1,dp);
+    //     int sum2=helper(cost,i-2,dp);
+    //     dp[i]=cost[i]+min(sum1,sum2);
+    //     return dp[i];
+    // }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n=cost.size();
-        vector<int>dp(n,-1);
-        return min(helper(cost,n-1,dp),helper(cost,n-2,dp));
+        // int n=cost.size();
+        // vector<int>dp(n,-1);
+        // return min(helper(cost,n-1,dp),helper(cost,n-2,dp));
+
+
+        //tabulation method
+        for(int i=2;i<cost.size();i++){
+            cost[i]+=min(cost[i-1],cost[i-2]);
+        }
+        return min(cost[cost.size()-1],cost[cost.size()-2]);
     }
 };
