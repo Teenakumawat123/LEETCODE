254c914c19cb3d7a397aeb10ef8a6293c70240e9
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-05-24 18:21:48 +0530

    Time: 96 ms (22.65%) | Memory: 25.6 MB (25.88%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_map<char,int>mp;
+        int c=0;
+        for(int i=0;i<word.size();i++){
+            if(word[i]>='a' && word[i]<='z'){
+                    mp[word[i]]=i;
+            }
+            if(word[i]>='A' && word[i]<='Z'){
+                if(mp.find(word[i])==mp.end()){
+                    mp[word[i]]=i;
+                }
+            }
+        }
+        for(auto x:mp) cout<<x.first<<x.second;
+        for(auto p:mp){
+            if(p.first>='a' && p.first<='z'){
+                if(mp.find(p.first-32)!=mp.end()){
+                    if(mp[p.first]<mp[p.first-32]) c++;
+                }       
+            }
+        }
+        
+        return c;
+    }
+};
