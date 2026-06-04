ae1ffb240b8eac9dc6af7e06e637d0946d2d8596
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-06-04 19:48:24 +0530

    Time: 91 ms (29.80%) | Memory: 85 MB (26.19%) - LeetSync

---
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        for(int x:arr){
+            mp[x]++;
+        }
+        if(mp.size()==1) return 1;
+        priority_queue<pair<int,int>>pq;
+        for(auto p:mp){
+            pq.push({p.second,p.first});
+        }
+        // while(pq.size()!=0){
+        //     cout<<pq.top().first;
+        //     pq.pop();
+        // }
+        int n=arr.size();
+        int size=0;
+        int ans=0;
+        while(pq.size()!=0){
+            size+=pq.top().first;
+            pq.pop();
+            if(size<n/2) ans++;
+            else{
+                if(size>=n/2) ans++;
+                break;
+            }
+        }
+        return ans;
+    }
+};
