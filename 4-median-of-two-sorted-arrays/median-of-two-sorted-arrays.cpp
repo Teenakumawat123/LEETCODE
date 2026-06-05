2636e61c7789ed20a5db052509df23c8f6f63f46
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-06-05 13:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 95.9 MB (23.93%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,37 +3,31 @@ public:
     double findMedianSortedArrays(vector<int>& n1, vector<int>& n2) {
         int m=n1.size();
         int n=n2.size();
-        vector<int>v(m+n);
-        int i=0,j=0,k=0;
+        vector<int>v;
+        int i=0,j=0;
         while(i<m && j<n){
             if(n1[i]<n2[j]){
-              v[k]=n1[i];
+              v.push_back(n1[i]);
               i++;
-              k++;
             }
             else{
-                v[k]=n2[j];
-                k++;
+                v.push_back(n2[j]);
                 j++;
             }
         }
         if(i==m){
             while(j<n){
-                v[k]=n2[j];
-                k++;
+                v.push_back(n2[j]);
                 j++;
             }
         }
         else{
             while(i<m){
-                v[k]=n1[i];
-                k++;
+                v.push_back(n1[i]);
                 i++;
             }
         }
-       for(int x :v){
-        cout<<x;
-       }
+
         double ans;
         if(v.size()%2!=0) ans=(double)v[v.size()/2];
         else ans=(double)(v[v.size()/2]+v[v.size()/2-1])/2;
