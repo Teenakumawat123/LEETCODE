5af7d38690cebad77529bf5e33a09a41a837f339
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-06-06 15:48:32 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (65.48%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& ans) {
+        // set<int>s;
+        // int res=0;
+        // for(int x:ans){
+        //     if(x==0) res++;
+        //     else s.insert(x);
+        // }
+        // for(int p:s){
+        //     res+=(p+1);
+        // }
+        // return res;
+
+        vector<int>v(1000,0);
+        int res=0;
+        for(int x:ans)
+        {
+            if(x==0) res++;
+            else v[x]++;
+            if(v[x]==x+1) {
+                res+=v[x];
+                v[x]=0;
+            }
+        }
+        for(int i=0;i<v.size();i++){
+            if(v[i]>0){
+                res+=(i+1);
+            }
+        }
+        return res;
+    }
+};
