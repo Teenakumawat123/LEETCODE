66f2cd7398c40ecef09b18c7656616588202982a
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-06-13 10:41:13 +0530

    Time: 401 ms (5.01%) | Memory: 69.9 MB (71.28%) - LeetSync

---
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool vowels(char ch){
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'|| ch=='u') return 1;
+        else return 0;
+    }
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& q) {
+        vector<int>v(words.size(),0);
+        for(int i=0;i<words.size();i++){
+            string s=words[i];
+            if(vowels(s[0]) && vowels(s.back())) v[i]=1;
+        }
+        for(int x:v) cout<<x<<" ";
+        for(int i=1;i<v.size();i++){
+            v[i]+=v[i-1];
+        }
+        cout<<endl;
+        for(int x:v) cout<<x<<" ";
+        vector<int>ans;
+        
+        for(int i=0;i<q.size();i++){
+            if(q[i][0]==0){
+                ans.push_back(v[q[i][1]]);
+            }
+            else{
+                ans.push_back(v[q[i][1]]-v[q[i][0]-1]);
+            }
+        }
+        return ans;
+    }
+};
