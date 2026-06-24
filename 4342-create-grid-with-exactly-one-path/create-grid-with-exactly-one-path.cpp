4aabdaad48192091027d419fe326f3f04d104e88
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-06-24 13:23:48 +0530

    Time: 3 ms (35.15%) | Memory: 11.2 MB (55.46%) - LeetSync

--- 4342-create-grid-with-exactly-one-path/create-grid-with-exactly-one-path.cpp
+++ 4342-create-grid-with-exactly-one-path/create-grid-with-exactly-one-path.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     vector<string> createGrid(int m, int n) {
-        vector<string>ans;
-        for(int i=0;i<m;i++){
-            string s="";
-            for(int j=0;j<n-1;j++){
-                if(i==0) s+='.';
-                else s+='#';
-            }
-            s+='.';
-            ans.push_back(s);
+        // vector<string>ans;
+        // for(int i=0;i<m;i++){
+        //     string s="";
+        //     for(int j=0;j<n-1;j++){
+        //         if(i==0) s+='.';
+        //         else s+='#';
+        //     }
+        //     s+='.';
+        //     ans.push_back(s);
+        // }
+        // return ans;
+
+        vector<string> ans(m, string(n, '#'));//this syntax will make one grid of size m*n .
+
+        ans[0] = string(n, '.');   //this will change first row into "...."
+
+        for (int i = 0; i < m; i++) {
+            ans[i].back() = '.'; //this will change last column into '.' this char.
         }
         return ans;
     }
