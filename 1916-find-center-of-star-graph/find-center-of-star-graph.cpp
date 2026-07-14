131bb5bf288a79e4d1a82266c716fee4bece6bef
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-07-14 20:21:26 +0530

    Time: 1244 ms (5.18%) | Memory: 121.1 MB (22.07%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& e) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<e.size();i++){
+            mp[e[i][0]]++;
+            mp[e[i][1]]++;
+        }
+        int ans=INT_MIN;
+        int res=INT_MIN;
+        for(auto x:mp){
+            cout<<x.first<<" "<<x.second<<" ";
+           if(ans<x.second) {
+            res=x.first;
+            ans=x.second;
+           }
+        }
+        return res;
+    }
+};
