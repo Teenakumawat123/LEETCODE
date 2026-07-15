84ac891987dd6eea77da3c03127e118ede7603e1
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-07-15 20:31:20 +0530

    Time: 7 ms (51.77%) | Memory: 15.2 MB (47.04%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+       /*
+       1st method
+               |
+              ***
+       4 step that we should follow
+           *create deep copy without random connection
+           *Alternate linking
+           *mark random pointer
+           *remove alternate connection
+       */
+
+       // second method -->using hashmap
+
+       unordered_map<Node*,Node*>mp;
+
+       Node* a=head;
+       Node* b=new Node(10);
+       Node* c=b;
+       while(a){
+        Node* d=new Node(a->val);
+        mp[a]=d;
+        b->next=d;
+        b=b->next;
+        a=a->next;
+       }
+       c=c->next;
+
+       for(auto x:mp){
+       Node*o=x.first;
+       Node*d=x.second;
+       if(o->random!=NULL){
+         Node*dr=mp[o->random];
+         d->random=dr;
+       }
+       }
+       return c;
+    }
+};
