26bcf5ffe7099e47c35e3b37f833f91574fb340d
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-07-16 17:23:26 +0530

    Time: 78 ms (43.32%) | Memory: 148.7 MB (97.39%) - LeetSync

---
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int gcd(int a, int b){
+        if(a==0) return b;
+        else return gcd(b%a,a);
+    }
+    long long gcdSum(vector<int>& nums) {
+        int maxi=nums[0];
+        for(int i=0;i<nums.size();i++){
+            if(i>0) maxi=max(maxi,nums[i]);
+            nums[i]=gcd(min(nums[i],maxi),max(nums[i],maxi));
+        }
+        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=nums.size()-1;
+        long long sum=0;
+        while(i<j){
+            sum+=gcd(min(nums[i],nums[j]),max(nums[i],nums[j]));
+            i++;
+            j--;
+        }
+        return sum;
+    }
+};
