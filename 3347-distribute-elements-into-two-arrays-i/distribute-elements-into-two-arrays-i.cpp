cc054ea54b4449b4d622b7835201da93e8c0e3b9
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-08-20 23:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (83.63%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -10,12 +10,9 @@ public:
             if(arr1[arr1.size()-1]>arr2[arr2.size()-1]) arr1.push_back(nums[i]);
             else arr2.push_back(nums[i]);
         }
-        for(int i=0;i<arr1.size();i++){
-            nums[i]=arr1[i];
+        for(int i=0;i<arr2.size();i++){
+            arr1.push_back(arr2[i]);
         }
-        for(int i=arr1.size();i<nums.size();i++){
-            nums[i]=arr2[i-arr1.size()];
-        }
-        return nums;
+        return arr1;
     }
 };
