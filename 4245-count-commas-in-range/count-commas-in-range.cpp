2d2db7431bfe5c1b80db35ff7f747b0d3fdac6d4
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-09-08 16:55:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.38%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int countCommas(int n) {
       if(n<1000) return 0;
-      else{
-        if((n/1000)==1) return (n%1000)+1;
-        else return (((n/1000)-1)*1000 +(n%1000)+1);
-      }
+      else return (((n/1000)-1)*1000 +(n%1000)+1);
     }
 };
