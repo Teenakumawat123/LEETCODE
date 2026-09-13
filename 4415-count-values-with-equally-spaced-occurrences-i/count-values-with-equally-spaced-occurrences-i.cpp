557d927a043598e06ef600484ebc1ecb8e5c901e
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-09-13 18:05:32 +0530

    Time: 15 ms (20.00%) | Memory: 35.3 MB (10.00%) - LeetSync

---
+++ 4415-count-values-with-equally-spaced-occurrences-i/count-values-with-equally-spaced-occurrences-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public: 
+    int countSpecialIntegers(vector<int>& nums) {
+        unordered_map<int,vector<int>>mp;
+        for(int i=0;i<nums.size();i++){ 
+            mp[nums[i]].push_back(i);
+        }
+        int ans=0;
+        
+        for(auto x:mp){
+            vector<int>v=x.second;
+            bool f=true;
+            if (v.size()==3){
+                 int s=abs(v[0]-v[1]);
+                 for(int i=1;i<v.size()-1;i++){
+                    if(abs(v[i]-v[i+1])!=s) {f=false; break;}
+                 }
+                 if(f) ans++;
+             }
+        }
+        return ans;
+    }
+};
