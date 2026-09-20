c3f6b7a5ee29ee7ce4a4adbeeb82dfc19573d405
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-09-20 11:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        int a=1;
+        //int a=1;
         int ans=0;
         for(int i=0;i<s.size();i++){
-            cout<<((122-s[i]+1)*a);
-         ans+=((122-s[i]+1)*a);
-         a++;
+         ans+=((122-s[i]+1)*(i+1));
         }
         return ans;
     }
