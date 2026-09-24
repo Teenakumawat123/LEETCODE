2e10217e96234280b19ae82e279b8c6422acd6b0
TEENA KUMAWAT <153281717+Teenakumawat123@users.noreply.github.com>
2026-09-24 10:27:15 +0530

    Time: 0 ms (100.00%) | Memory: 31 MB (52.36%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-int digitsum(int x){
-    int sum=0;
-    string s=to_string(x);
-    for(int i=0;i<s.size();i++){
-     sum+=(s[i]-48);
-    }
-    return sum;
-}
     int smallestIndex(vector<int>& nums) {
-        int idx=INT_MAX;
+        
         for(int i=0;i<nums.size();i++){
-           int sum=digitsum(nums[i]);
-           if(sum==i) idx=min(idx,i);
+             int sum=0;
+             while(nums[i]){
+                sum+=(nums[i]%10);
+                nums[i]/=10;
+             }
+             if(sum==i) return i;
         }
-        if(idx==INT_MAX) return -1;
-        return idx;
+        return -1;
     }
 };
